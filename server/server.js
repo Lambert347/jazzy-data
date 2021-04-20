631b11a5136b7aec88b56e315f4cae0e0471aae9
Ian Lambert <lambe347@umn.edu>
2021-04-19 20:50:26 -0500

    pool and pg setup

--- server/server.js
+++ server/server.js
@@ -7,6 +7,16 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
+const pg = require('pg');
+const { Pool } = require('pg');
+const pool = new Pool({
+    database: 'jazzy_sql',
+    host: 'localhost',
+    port: '5432',
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
